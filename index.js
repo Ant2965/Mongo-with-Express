f9c791f1c5aef561ae5850f0748ad460f7649ed6
Ant2965 <antrikshbahri@gmail.com>
2024-02-24 00:26:12 +0530

    setting up the environment done with adding the vlaue to db using models and schema and lastly chats.save()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const path = require('path')
 const chat = require("./models/chat.js")
 const app = express();
-
+app.use(express.urlencoded({extended: true}))
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs");
@@ -39,6 +39,31 @@ app.get("/chats",async(req,res)=>{
 app.get("/", (req, res) => {
     res.send("root work")
 })
+
+
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs")
+})
+
+//route
+
+app.get("/chats", (req,res)=>{
+
+    let {from,to,msg}=req.body;
+
+    let newchat = new chat({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    })
+
+    console.log(newchat)
+    res.send("works newchat")
+
+})
+
+
 
 app.listen(8100, () => {
     console.log("server on port 6000")
